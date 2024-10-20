e5adfa1f21bbc744a5814176a7f5a02fb46fc837
KIMbyeo-ri <ehdgo70@naver.com>
2024-10-20 20:06:55 +0900

    ,
    ,

---
+++ 레포트 2-1 switch/레포트 2-1 switch/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    printf("정수를 입력하세요:");
+    scanf_s("%d", &num);
+
+    switch (num % 2)
+    {
+    case 0:
+        printf("%d는 짝수 입니다.\n", num);
+        break;
+
+    case 1:
+        printf("%d는 홀수 입니다.\n", num);
+        break;
+    }
+
+
+    return 0;
+}
