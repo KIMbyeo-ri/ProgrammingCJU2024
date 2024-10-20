5d68e0cd3c67e8c95b24abb068b462c7d58e059a
KIMbyeo-ri <ehdgo70@naver.com>
2024-10-20 21:36:22 +0900

    ,
    ,

---
+++ 레포트 2-3 switch/레포트 2-3 switch/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float score;
+    char grade;
+
+    printf("정적 입력(0.0~100.0): ");
+    scanf_s("%f", &score);
+
+    switch((int)score / 10) {
+    case 10 :
+    case 9 :
+            grade = 'A';
+            break;
+
+    case 8:
+        grade = 'B';
+        break;
+
+    case 7:
+        grade = 'C';
+        break;
+
+    case 6:
+        grade = 'C';
+        break;
+
+    case 5:
+        grade = 'D';
+        break;
+
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        grade = 'F';
+        break;
+        default:
+            printf("잘못된 점수입니다.\n");
+            return 1;
+    }
+
+    printf("학점: %c\n", grade);
+    return 0;
+}
