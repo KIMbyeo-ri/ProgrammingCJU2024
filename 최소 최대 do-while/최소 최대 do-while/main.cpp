b0cdd7f38984b2ff00db83c55d889544795ab455
KIMbyeo-ri <ehdgo70@naver.com>
2024-10-23 17:18:49 +0900

    ,
    ,

--- 최소 최대 do-while/최소 최대 do-while/main.cpp
+++ 최소 최대 do-while/최소 최대 do-while/main.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2, gcd, lcm, a, b;
+    int num1;
+    int num2;
+    int gcd;
+    int lcm;
+    int a;
+    int b;
 
     // 두 숫자 입력 받기
     printf("두 숫자를 입력하세요: ");
@@ -10,19 +15,19 @@ int main(void) {
     a = num1;
     b = num2;
 
-    // 최대공약수 계산 (유클리드 호제법 사용, do-while 문으로 변경)
+    //최대공약수 계산 (유클리드 호제법 사용)
     do {
         int temp = b;
         b = a % b;
         a = temp;
-    } while (b != 0); // b가 0이 될 때까지 반복
+    } while (b != 0); //b가 0이 될 때까지 반복
 
     gcd = a;
 
-    // 최소공배수 계산
+    //최소공배수 계산
     lcm = (num1 * num2) / gcd;
 
-    // 결과 출력
+    //결과 출력
     printf("최대공약수(GCD): %d\n", gcd);
     printf("최소공배수(LCM): %d\n", lcm);
 
